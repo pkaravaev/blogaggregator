94624c09d5659c45b97ddf32c0bd3a50c3490843
pavloid <karavaev.pavel.sergeevich@gmail.com>
2018-04-08 20:00:10 +0300

    add index controller

---
+++ src/main/java/com/pkaravaev/service/ItemService.java
@@ -0,0 +1,16 @@
+package com.pkaravaev.service;
+
+import com.sun.tools.javac.jvm.Items;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+
+
+    public List<Items> getItems() {
+
+        return null;
+    }
+}

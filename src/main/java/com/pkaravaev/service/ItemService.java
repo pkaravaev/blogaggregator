ca0626cad009c4e27b4c0a3ec4e7baa31d37c1a8
pavloid <karavaev.pavel.sergeevich@gmail.com>
2018-04-08 20:01:04 +0300

    add index service

--- src/main/java/com/pkaravaev/service/ItemService.java
+++ src/main/java/com/pkaravaev/service/ItemService.java
@@ -1,6 +1,11 @@
 package com.pkaravaev.service;
 
+import com.pkaravaev.model.Item;
+import com.pkaravaev.repository.ItemRepository;
 import com.sun.tools.javac.jvm.Items;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,8 +14,10 @@ import java.util.List;
 public class ItemService {
 
 
-    public List<Items> getItems() {
+    @Autowired
+    private ItemRepository itemRepository;
 
-        return null;
+    public List<Item> getItems() {
+        return itemRepository.findAll(new PageRequest(0, 20, Sort.Direction.DESC, "publishedDate")).getContent();
     }
 }

48a08baf792af662bd5f3e567798606d31ef9740
pavloid <karavaev.pavel.sergeevich@gmail.com>
2018-04-09 20:58:22 +0300

    add  RSS service

---
+++ src/main/java/com/pkaravaev/service/RssService.java
@@ -0,0 +1,24 @@
+package com.pkaravaev.service;
+
+
+import com.pkaravaev.exception.RssException;
+import com.pkaravaev.model.Item;
+import org.springframework.stereotype.Service;
+
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+import java.util.List;
+
+@Service
+public class RssService {
+
+
+    public List<Item> getItems(File file) throws RssException {
+        return getItems(new StreamSource(file));
+    }
+
+    public List<Item> getItems(String url) throws RssException {
+        return getItems(new StreamSource(url));
+    }
+
+}

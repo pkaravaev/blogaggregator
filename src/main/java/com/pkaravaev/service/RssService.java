0bb843d329c78855700c887adefa9f8e26c1db55
pavloid <karavaev.pavel.sergeevich@gmail.com>
2018-04-16 17:37:13 +0300

    add rss service

--- src/main/java/com/pkaravaev/service/RssService.java
+++ src/main/java/com/pkaravaev/service/RssService.java
@@ -3,22 +3,59 @@ package com.pkaravaev.service;
 
 import com.pkaravaev.exception.RssException;
 import com.pkaravaev.model.Item;
+import com.pkaravaev.rss.ObjectFactory;
 import org.springframework.stereotype.Service;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 @Service
 public class RssService {
 
-//
-//    public List<Item> getItems(File file) throws RssException {
-//        return getItems(new StreamSource(file));
-//    }
-//
-//    public List<Item> getItems(String url) throws RssException {
-//        return getItems(new StreamSource(url));
-//    }
+    public List<Item> getItems(File file) throws RssException {
+        return getItems(new StreamSource(file));
+    }
 
+    public List<Item> getItems(String url) throws RssException {
+        return getItems(new StreamSource(url));
+    }
+
+    private List<Item> getItems(Source source) throws RssException {
+        ArrayList<Item> list = new ArrayList<Item>();
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            JAXBElement<TRss> jaxbElement = unmarshaller.unmarshal(source, TRss.class);
+            TRss rss = jaxbElement.getValue();
+
+            List<TRssChannel> channels = rss.getChannel();
+            for (TRssChannel channel : channels) {
+                List<TRssItem> items = channel.getItem();
+                for (TRssItem rssItem : items) {
+                    Item item = new Item();
+                    item.setTitle(rssItem.getTitle());
+                    item.setDescription(rssItem.getDescription());
+                    item.setLink(rssItem.getLink());
+                    Date pubDate = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH).parse(rssItem.getPubDate());
+                    item.setPublishedDate(pubDate);
+                    list.add(item);
+                }
+            }
+        } catch (JAXBException e) {
+            throw new RssException(e);
+        } catch (ParseException e) {
+            throw new RssException(e);
+        }
+        return list;
+    }
 }

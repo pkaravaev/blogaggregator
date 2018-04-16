82f784ee2806ec7594c2d0d219214ddc1aabc3e4
pavloid <karavaev.pavel.sergeevich@gmail.com>
2018-04-16 17:35:25 +0300

    add rss

---
+++ src/main/java/com/pkaravaev/rss/TCategory.java
@@ -0,0 +1,69 @@
+package com.pkaravaev.rss;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "tCategory", propOrder = {
+        "value"
+})
+public class TCategory {
+
+    @XmlValue
+    protected String value;
+    @XmlAttribute(name = "domain")
+    protected String domain;
+
+    /**
+     * Gets the value of the value property.
+     *
+     * @return
+     *     possible object is
+     *     {@link String }
+     *
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * Sets the value of the value property.
+     *
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    /**
+     * Gets the value of the domain property.
+     *
+     * @return
+     *     possible object is
+     *     {@link String }
+     *
+     */
+    public String getDomain() {
+        return domain;
+    }
+
+    /**
+     * Sets the value of the domain property.
+     *
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *
+     */
+    public void setDomain(String value) {
+        this.domain = value;
+    }
+
+}
